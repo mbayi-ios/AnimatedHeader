847cd0945b3303fa1d1dd8293c86937892ba7b7d
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-10-17 13:59:36 +0300

    Initial Commit

---
+++ AnimatedHeader/AnimatedHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedHeaderApp.swift
+//  AnimatedHeader
+//
+//  Created by Amby on 17/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
